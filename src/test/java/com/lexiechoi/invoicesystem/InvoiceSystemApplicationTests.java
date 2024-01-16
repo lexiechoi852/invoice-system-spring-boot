cfb367983ddd8a4f16bfe3fcc30125bd51276e72
Lexie Choi <lexiechoi852@gmail.com>
2024-01-16 07:22:44 -0500

    Initial Commit

---
+++ src/test/java/com/lexiechoi/invoicesystem/InvoiceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexiechoi.invoicesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
