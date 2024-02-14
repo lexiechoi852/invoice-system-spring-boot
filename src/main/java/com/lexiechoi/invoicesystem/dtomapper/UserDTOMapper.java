f2c52ce4d83acd1cd1e253014fe4e28dff8651a3
Lexie Choi <lexiechoi852@gmail.com>
2024-02-13 19:59:07 -0500

    feat: added role and user dtos

---
+++ src/main/java/com/lexiechoi/invoicesystem/dtomapper/UserDTOMapper.java
@@ -0,0 +1,21 @@
+package com.lexiechoi.invoicesystem.dtomapper;
+
+import com.lexiechoi.invoicesystem.domain.User;
+import com.lexiechoi.invoicesystem.dto.UserDTO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDTOMapper {
+    public static UserDTO fromUser(User user) {
+        UserDTO userDTO = new UserDTO();
+        BeanUtils.copyProperties(user, userDTO);
+        return userDTO;
+    }
+
+    public static User toUser(UserDTO userDTO) {
+        User user = new User();
+        BeanUtils.copyProperties(userDTO, user);
+        return user;
+    }
+}
